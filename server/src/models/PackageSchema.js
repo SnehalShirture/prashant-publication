02cdab02686971fbf46ff4bbdc10090a73599f95
ashishBendale777 <36222108+ashishBendale777@users.noreply.github.com>
2025-02-16 17:47:07 +0530

    added

--- server/src/models/PackageSchema.js
+++ server/src/models/PackageSchema.js
@@ -1,4 +1,5 @@
 import mongoose from "mongoose";
+import { Book } from "./BookSchema.js";
 
 const PackageSchema = mongoose.Schema({
     category: {
@@ -13,8 +14,8 @@ const PackageSchema = mongoose.Schema({
         type: Boolean,
     },
     booksIncluded: [
-        { type: mongoose.Schema.Types.ObjectId ,
-            
+        {
+            type: mongoose.Schema.Types.ObjectId, ref: "Book"
         }
     ],
     prices: [
@@ -24,5 +25,21 @@ const PackageSchema = mongoose.Schema({
         }
     ]
 })
+
+
+
+// Pre-save middleware to populate booksIncluded before saving
+PackageSchema.pre("save", async function (next) {
+    if (this.isNew || this.isModified("category") || this.isModified("academicYear")) {
+        try {
+            // Find books that match the category and academicYear
+            const books = await Book.find({ category: this.category, academicYear: this.academicYear }).select("_id");
+            this.booksIncluded = books.map(book => book._id);
+        } catch (error) {
+            return next(error);
+        }
+    }
+    next();
+});
 
 export const Package = mongoose.model("Package", PackageSchema)
